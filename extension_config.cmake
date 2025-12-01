c23aec4936406665a21467d0096e32cd1cd80f37
Uri Shavit <uri.shavit@versatile.ai>
2025-12-01 20:14:25 +0200

    code clean

--- extension_config.cmake
+++ extension_config.cmake
@@ -6,9 +6,8 @@ duckdb_extension_load(unity_catalog
     LOAD_TESTS
 )
 
-# Delta extension disabled for testing without delta
-# duckdb_extension_load(delta
-#     GIT_URL https://github.com/duckdb/duckdb-delta
-#     GIT_TAG 48168a8ff954e9c3416f3e5affd201cf373b3250
-#     SUBMODULES extension-ci-tools
-# )
+duckdb_extension_load(delta
+    GIT_URL https://github.com/duckdb/duckdb-delta
+    GIT_TAG 48168a8ff954e9c3416f3e5affd201cf373b3250
+    SUBMODULES extension-ci-tools
+)
