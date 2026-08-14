91b8839d8d4a31de621cd69c6ece70245f6b6f23
ben fleis <benfleis@users.noreply.github.com>
2026-08-14 09:21:01 +0200

    grab latest duckdb/main + patch

--- extension_config.cmake
+++ extension_config.cmake
@@ -9,6 +9,6 @@ duckdb_extension_load(unity_catalog
 # NOTE: replace with SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../delta for local dev
 duckdb_extension_load(delta
     GIT_URL https://github.com/duckdb/duckdb-delta
-    GIT_TAG ccd80e5b9c3a7263da1c0b0c810d404563300a41
+    GIT_TAG d42993d5684946bcb7efacb7a4102021687623f7
     SUBMODULES extension-ci-tools
 )
